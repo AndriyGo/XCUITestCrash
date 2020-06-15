cf8e376bf750b1d37b073301ba53c26ef5c5163b
Andriy Gordiychuk <andriy@glenmax.com>
2020-06-15 17:50:26 +0300

    Initial Commit

---
+++ XCUITestCrash/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XCUITestCrash
+//
+//  Created by Andriy Gordiychuk on 15/06/2020.
+//  Copyright © 2020 Andriy Gordiychuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
